cdd5d0acfb02b0dae0e239e82dbba206d3acb675
boogaeye <bradleigh.christensen@makt3k.com>
2020-11-03 13:21:46 -0800

    Load Method and some vars

--- SomeAmongUsPlugin/SomeAmongUsPlugin.cs
+++ SomeAmongUsPlugin/SomeAmongUsPlugin.cs
@@ -15,9 +15,20 @@ namespace SomeAmongUsPlugin
     [BepInProcess("Among Us.exe")]
     public class BruhmongUs : BasePlugin
     {
+        public ConfigEntry<int> playerMax { get; set; }
+        public ConfigEntry<int> imposterMax { get; set; }
+        public ConfigEntry<int> killCooldown { get; set; }
+        public ConfigEntry<float> walkSpeed { get; set; }
+        public ConfigEntry<bool> allowInfection { get; set; }
+        public ConfigEntry<bool> unknownImposters { get; set; } //all imposters will have white names and can kill each other if enabled
         public override void Load()
         {
-
+            playerMax = Config.Bind("Custom Room", "Max Players", 10);
+            imposterMax = Config.Bind("Custom Room", "Max Imposters", 2);
+            killCooldown = Config.Bind("Custom Room", "Kill Cooldown", 10);
+            walkSpeed = Config.Bind("Custom Room", "Walking Speed", 1.5f);
+            allowInfection = Config.Bind("Custom Gamemode", "Infection", false);
+            unknownImposters = Config.Bind("Imposter Settings", "Unknown Imposters", false);
         }
     }
 }
