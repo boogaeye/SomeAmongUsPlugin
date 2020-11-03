58f61ed0db662e4dd09c1c9bb1099d9b2bbd3b72
boogaeye <bradleigh.christensen@makt3k.com>
2020-11-03 13:01:10 -0800

    Add project files.

---
+++ SomeAmongUsPlugin/SomeAmongUsPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using UnityEngine;
+
+namespace SomeAmongUsPlugin
+{
+    [BepInPlugin("pl.BoogaEye.BruhmongUs", "BruhmongUs", "0.0.1")]
+    [BepInProcess("Among Us.exe")]
+    public class BruhmongUs : BasePlugin
+    {
+        public override void Load()
+        {
+
+        }
+    }
+}
